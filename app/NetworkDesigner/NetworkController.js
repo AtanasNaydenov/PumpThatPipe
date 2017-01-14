f74f3f96aa526e41afcf34b31138b71cb829d495
Dmitry <dmitry-orlov@hotmail.com>
2017-01-14 22:44:52 +0100

    some more stuff the update network method is yet not done

--- app/NetworkDesigner/NetworkController.js
+++ app/NetworkDesigner/NetworkController.js
@@ -19,18 +19,23 @@ class NetworkController {
     // A method that attempts adding a component to the network with the give x and y
     // returns a boolean representing the result of the action 
     putComponent(x, y) {
-        let _isDrawable = this.isDrawable(x, y);
-        if (_isDrawable) {
+        let _isDrawableResp = this.isDrawable(x, y);
+        if (_isDrawableResp) {
             // check if returns something
             this.SelectedTemplatePart.SetLocation(x, y);
         }
-        return _isDrawable;
+        return _isDrawableResp;
     }
 
     // Checks the if the x and y coordinates are available for placing smthng
     // returns a boolean result
     isDrawable(x, y) {
-        return false;
+        for(let i = 0; i<this.Parts.length;i++){
+            if(this.Parts[i].Contains(x,y)){
+                return false; // not drawable!
+            }
+        }
+        return true; // since this line was reached, none of the components contained the x and y
     }
 
     // adds a new pipeline based on the components a and b as parameters
@@ -51,13 +56,22 @@ class NetworkController {
 
     // updates the global settings of the NC 
     modifyGlobalSettings(settings) {
+        //TO DO
         return false;
     }
 
     // attempts to remove a given component 
     remove(part, isPipeline) {
+        let _removeResult = false;
+
+        if (!(part instanceof Component() 
+            || part instanceof Pipeline())){
+                // neither a component or a pipeline - aaaargh, wrong-wrong-wrong.
+                return false;
+            }
+        // at least one of those
         if (!isPipeline) {
-            try {
+            try { // try enables skipping the check for being a component by just assuming it is
                 _listOfPLtoDelete = part.RemoveAllPipelines();
                 //
                 if (_listOfPLtoDelete.length > 0) {
@@ -65,16 +79,32 @@ class NetworkController {
                         this.removeFromList(_listOfPLtoDelete[i]);
                     }
                 }
-                return true;
+                _removeResult = this.removeFromList(part); // maybe something goes wrong, huh?
             } catch (e) {
                 console.log("oups, something went wrong" + e)
-                return false;
+                _removeResult = false;
             }
+        } else {
 
+            // actually the pipeline is to be deleted
+            if(part instanceof Pipeline()){ // just to be sure
+                let detachResult = part.Detach();
+                _removeResult = this.removeFromList(part);
 
+            }else{
+
+                console.log('tried to delete not a pipeiple as if it was a pipeline...');
+                _removeResult = false;
+            }
         }
-        return false;
+        
+        if(_removeResult){
+            this.updateNetwork
+        }      
+        return _removeResult;
     }
+
+
 
     // attemots to remove a pipeline
     // remove(pl) {
@@ -83,8 +113,15 @@ class NetworkController {
     // overloading is not nice for JS
 
     // removes a part from the list of parts
-    removeFromList(part) {
-
+    removeFromList(_partToDelete) {
+        let _indexToDelete = 0;
+        for (let i = 0; i < this.Parts.length; i++) {
+            if (this.Parts[i].id == _partToDelete.Id) {
+                _indexToDelete = i;
+                break;
+            }
+        }
+        return (this.Parts.splice(_indexToDelete, 1)).length > 0;
     }
 
     // void
@@ -99,12 +136,14 @@ class NetworkController {
 
     // where params is a JSON object with parameters to change
     updateExistingSelectedPart(params) {
+        // to do
         return false;
     }
 
     // void
     setNewMaximumPipelineFlow(flowAmount) {
-
+        // would it work? 
+        Pipeline.SetMaxflow(flowAmount);
     }
 
     detectComponent(x, y) {
